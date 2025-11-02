95de27bfd4cd4ec2fda8c8720ade00ea2b6d39ac
Maksym Bondarchuk <belkamax05@gmail.com>
2025-11-02 01:12:25 +0000

    latest

--- include-zshenv.sh
+++ include-zshenv.sh
@@ -2,10 +2,10 @@
 __CURRENT_FILE=${(%):-%N}
 __CURRENT_DIR=$(realpath $(dirname $__CURRENT_FILE))
 
-SHULKER_DIR=$__CURRENT_DIR
+SHULKER_CLI_DIR=$__CURRENT_DIR
 
-source "$SHULKER_DIR/functions/_configure/configure-functions"
-configure-functions "$SHULKER_DIR/functions"
+source "$SHULKER_CLI_DIR/functions/_configure/configure-functions"
+configure-functions "$SHULKER_CLI_DIR/functions"
 
 SHU_START_TIME=$(get-time-ms)
 
@@ -19,7 +19,7 @@ override-nvm-load
 
 config-emit
 
-export PATH="$PATH:$SHULKER_DIR/bin"
+export PATH="$PATH:$SHULKER_CLI_DIR/bin"
 export PATH="$HOME/.nvm/versions/node/v20.14.0/bin:$PATH"
 
 echo-trace-project "shulker-cli [zshenv]" "$__CURRENT_DIR"
